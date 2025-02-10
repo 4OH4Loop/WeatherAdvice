090949e803cb56659a3cc62f9f963ca0f1b43903
4OH4Loop <semperchuck@gmail.com>
2025-02-10 18:30:47 -0500

    Initial Commit

---
+++ WeatherAdvice/WeatherAdviceApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAdviceApp.swift
+//  WeatherAdvice
+//
+//  Created by Carolyn Ballinger on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAdviceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
